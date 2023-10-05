376b0fa2d1a3e69fb07c654ab74eb800a10b943a
aselvakumar525 <aselvakumar525@gmail.com>
2023-10-05 15:59:08 +0530

    Success

---
+++ SK/newapp/static/ssssk table.py
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Slot Timetable</title>
+</head>
+<body>
+<center>
+<img src="logo.png" height="100" width="540">
+</center>
+<br>
+<table align="center" width="540" cellspacing="2" cellpadding="4" border="5" bgcolor="skyblue">
+<caption><b>SLOT TIME TABLE - Selva Kumar A (212222110042)</b></caption>
+<tr align="center">
+<th bgcolor="yellow">Day/Time</th>
+<th bgcolor="yellow">Monday</th>
+<th bgcolor="yellow">Tuesday</th>
+<th bgcolor="yellow">Wednesday</th>
+<th bgcolor="yellow">Thursday</th>
+<th bgcolor="yellow">Friday</th>
+<th bgcolor="yellow">Saturday</th>
+</tr>
